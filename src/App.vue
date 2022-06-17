05f2e1c1a3a11c40b609744c9d5a2f9753b8dc58
Jakub Vrtiška <jkbvrtiska@gmail.com>
2022-06-17 08:07:22 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <carousel @next="next" @prev="prev">
+      <carousel-slide
+        v-for="(slide, index) in slides"
+        :key="slide"
+        :index="index"
+        :visibleSlide="visibleSlide"
+      >
+        <img :src="slide" />
+      </carousel-slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Carousel from "./components/Carousel";
+import CarouselSlide from "./components/CarouselSlide";
 export default {
-  name: 'App',
+  data() {
+    return {
+      slides: [
+        "https://picsum.photos/id/233/600/350",
+        "https://picsum.photos/id/234/600/350",
+        "https://picsum.photos/id/235/600/350",
+        "https://picsum.photos/id/231/600/350",
+        "https://picsum.photos/id/238/600/350",
+      ],
+      visibleSlide: 0,
+    };
+  },
+  cumputed: {
+    slidesLen() {
+      return this.slides.lenght;
+    },
+  },
+  methods: {
+    next() {
+      if (this.visibleSlide >= this.slidesLen - 1) {
+        this.visibleSlide = 0;
+      } else {
+        this.visibleSlide++;
+      }
+    },
+    prev() {
+      if (this.visibleSlide <= 0) {
+        this.visibleSlide = this.slidesLen - 1;
+      } else {
+        this.visibleSlide--;
+      }
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    Carousel,
+    CarouselSlide,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.app {
+  display: flex;
+  justify-content: center;
 }
 </style>
